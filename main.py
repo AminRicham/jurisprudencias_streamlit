acb1ce8c2985ab19cc9fffce7c1ba56d34b7b4b1
Amin Richam <110353705+AminRicham@users.noreply.github.com>
2025-10-27 15:53:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -83,18 +83,23 @@ if st.button("Excluir"):
 # Exibição de registros
 st.subheader("Lista de Jurisprudências")
 
-# Mostrar dados
-if data["jurisprudencias"]:
-    for rec in data["jurisprudencias"]:
-        st.write(f"**Número:** {rec['numero']}")
-        st.write(f"**Tribunal:** {rec['tribunal']}")
-        st.write(f"**Data:** {rec['data']}")
-        st.write(f"**Tipo:** {rec['tipo']}")
-        st.write(f"**Relator:** {rec['relator']}")
-        st.write(f"**Assuntos:** {', '.join(rec['assuntos'])}")
-        st.write(f"**Palavras:** {', '.join(rec['palavras'])}")
-        st.write(f"**Ementa:** {rec['ementa']}")
-        st.write(f"**Link:** {rec['link']}")
-        st.text("---")
-else:
-    st.write("Nenhum registro encontrado.")
+def show_data():
+    # Mostrar dados
+    if data["jurisprudencias"]:
+        for rec in data["jurisprudencias"]:
+            st.write(f"**Número:** {rec['numero']}")
+            st.write(f"**Tribunal:** {rec['tribunal']}")
+            st.write(f"**Data:** {rec['data']}")
+            st.write(f"**Tipo:** {rec['tipo']}")
+            st.write(f"**Relator:** {rec['relator']}")
+            st.write(f"**Assuntos:** {', '.join(rec['assuntos'])}")
+            st.write(f"**Palavras:** {', '.join(rec['palavras'])}")
+            st.write(f"**Ementa:** {rec['ementa']}")
+            st.write(f"**Link:** {rec['link']}")
+            st.text("---")
+    else:
+        st.write("Nenhum registro encontrado.")
+
+with st.sidebar:    
+    show_data()
+
