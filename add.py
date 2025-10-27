6be81f31fa159640dad4e12da62a1c1bbfb75eee
Amin Richam <110353705+AminRicham@users.noreply.github.com>
2025-10-27 16:23:27 -0300

    Update add.py

--- add.py
+++ add.py
@@ -1,15 +1,8 @@
-# Mostrar dados
-if data["jurisprudencias"]:
-    for rec in data["jurisprudencias"]:
-        st.write(f"**Número:** {rec['numero']}")
-        st.write(f"**Tribunal:** {rec['tribunal']}")
-        st.write(f"**Data:** {rec['data']}")
-        st.write(f"**Tipo:** {rec['tipo']}")
-        st.write(f"**Relator:** {rec['relator']}")
-        st.write(f"**Assuntos:** {', '.join(rec['assuntos'])}")
-        st.write(f"**Palavras:** {', '.join(rec['palavras'])}")
-        st.write(f"**Ementa:** {rec['ementa']}")
-        st.write(f"**Link:** {rec['link']}")
-        st.text("---")
-else:
-    st.write("Nenhum registro encontrado.")
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
